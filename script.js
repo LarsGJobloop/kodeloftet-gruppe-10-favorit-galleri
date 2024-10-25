5bba78111a03837031b9867cdbb8aafc338c3b1d
LarsGJobloop <lars.gunnar@jobloop.no>
2024-10-25 12:51:01 +0200

    add event handlers to all image cards

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log('Hello!');
+// Hent ut all "korten" fra HTML
+let allImageCards = document.querySelectorAll(".card");
+
+// For hvert av kortene, legg til en favoritt funksjon
+for (let imageCard of allImageCards) {
+  // Hent ut favoritt knappen
+  let favoritButton = imageCard.querySelector("button");
+
+  // Så legger vi til en egen funksjon, arrow funksjon "() => {}"
+  favoritButton.addEventListener("click", () => {
+    toggleFavorited(imageCard);
+  });
+}
+
+// Favoritt funksjonen
+function toggleFavorited(card) {
+  console.log(card);
+}
